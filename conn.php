c82da0aba89e582acf96009232e5b848c46efa90
zen <eiffenarjen26@gmail.com>
2026-05-07 12:57:19 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,16 +1,20 @@
 <?php
-// Mengambil data dari environment variables Railway
-$host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
-$user = getenv('MYSQLUSER') ?: 'root';
-$pass = getenv('MYSQLPASSWORD') ?: 'LZstNChgUtUIzpLIpzjVtJVusCMZscPX';
-$db   = getenv('MYSQLDATABASE') ?: 'railway';
+
+header("Content-Type: application/json");
+
+$host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
+$user = getenv('MYSQLUSER') ?: "root";
+$pass = getenv('MYSQLPASSWORD') ?: "LZbymHnnzmaaxbqhtTqfkWxMPuQQnIXG";
+$db   = getenv('MYSQLDATABASE') ?: "railway";
 $port = getenv('MYSQLPORT') ?: 3306;
 
-// Melakukan koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 
-// Cek koneksi
 if (!$koneksi) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die(json_encode([
+        "status" => false,
+        "message" => "Koneksi gagal",
+        "error" => mysqli_connect_error()
+    ]));
 }
 ?>
